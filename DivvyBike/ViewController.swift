8fb9b1d57fda91db7a0551685c80f56ac667a6a1
Brandon Kim <bkim2192@stu.d214.org>
2019-09-03 14:18:38 -0500

    end of today

--- DivvyBike/ViewController.swift
+++ DivvyBike/ViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
-    var stationsArray:[String] = ["hi"]
+    var stationsArray:[String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
+        JSON()
         // Do any additional setup after loading the view.
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -29,8 +30,31 @@ class ViewController: UIViewController, UITableViewDataSource {
         
         return cell
     }
-    
-    
+    func JSON() {
+        let url = URL(string: "https://data.cityofchicago.org/resource/aavc-b2wj.json")!
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let data = data {
+                let jsonObject = try? JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                
+                var outerLayer = jsonObject as! NSArray
+                for section in outerLayer {
+                    let section = section as! NSDictionary
+                    let name = section.object(forKey: "station_name") as!String
+                    self.stationsArray.append(name)
+                }
+                
+                
+                DispatchQueue.main.async {
+                    self.navigationItem.title = "Weather For: "
+                    self.tableView.reloadData()
+                }
+                
+                
+            }
+            
+        }
+    .resume()
+    }
     
     
     
